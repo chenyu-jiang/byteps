9f9bcf070363945f483407951c931e51f122b2ad
chenyu-jiang <1155092088@link.cuhk.edu.hk>
2020-04-13 23:41:59 +0800

    fixed bug

--- byteps/server/server.cc
+++ byteps/server/server.cc
@@ -24,7 +24,7 @@ using namespace ps;
 
 std::mutex server_trace_mu_;
 
-void LogServerTrace(const char* startend , uint64_t key, const char* msg) {
+void LogServerTrace(const std::string& startend , uint64_t key, const std::string& msg) {
   static std::ofstream log_file;
   static const char* dict_path = std::getenv("BYTEPS_SERVER_LOG_PATH");
   if (dict_path != NULL) {
